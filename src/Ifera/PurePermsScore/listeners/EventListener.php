3610f2f600a158969f659704252e7f4742546702
RifqyAja <156778697+rydevs29@users.noreply.github.com>
2025-12-03 14:58:14 +0700

    Update EventListener.php

--- src/Ifera/PurePermsScore/listeners/EventListener.php
+++ src/Ifera/PurePermsScore/listeners/EventListener.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace Ifera\PurePermsScore\listeners;
 
 use _64FF00\PurePerms\event\PPGroupChangedEvent;
-use _64FF00\PurePerms\PurePerms;
 use Ifera\PurePermsScore\Main;
 use Ifera\ScoreHud\event\PlayerTagsUpdateEvent;
 use Ifera\ScoreHud\scoreboard\ScoreTag;
@@ -23,30 +22,41 @@ class EventListener implements Listener {
 
     public function onJoin(PlayerJoinEvent $event): void {
         $player = $event->getPlayer();
+        // Cek dasar, meski biasanya di onJoin player pasti online
         if (!$player->isOnline()) {
             return;
         }
         $this->sendUpdate($player);
     }
 
+    /**
+     * @param PPGroupChangedEvent $event
+     * @phpstan-ignore-next-line
+     */
     public function onGroupChange(PPGroupChangedEvent $event): void {
+        // Method getPlayer() di PurePerms mungkin mengembalikan IPlayer/OfflinePlayer
+        // Jadi kita harus memastikan itu adalah Player online
         $player = $event->getPlayer();
+        
         if (!$player instanceof Player || !$player->isOnline()) {
             return;
         }
         $this->sendUpdate($player);
     }
 
-    // Mendeteksi perubahan prefix/suffix melalui chat sebagai solusi sementara
+    // Mendeteksi perubahan prefix/suffix melalui chat
     public function onPlayerChat(PlayerChatEvent $event): void {
         $this->sendUpdate($event->getPlayer());
     }
 
     private function sendUpdate(Player $player): void {
-        (new PlayerTagsUpdateEvent($player, [
-            new ScoreTag("ppscore.rank", $this->plugin->getPlayerRank($player)),
-            new ScoreTag("ppscore.prefix", $this->plugin->getPrefix($player)),
-            new ScoreTag("ppscore.suffix", $this->plugin->getSuffix($player))
-        ]))->call();
+        // Pastikan ScoreHud terinstall agar class ini ada
+        if (class_exists(PlayerTagsUpdateEvent::class) && class_exists(ScoreTag::class)) {
+            (new PlayerTagsUpdateEvent($player, [
+                new ScoreTag("ppscore.rank", $this->plugin->getPlayerRank($player)),
+                new ScoreTag("ppscore.prefix", $this->plugin->getPrefix($player)),
+                new ScoreTag("ppscore.suffix", $this->plugin->getSuffix($player))
+            ]))->call();
+        }
     }
 }
